e10952030538127f4a7eb4238f242d53879cba44
coretl <coretl@users.noreply.github.com>
2025-12-05 11:59:11 +0000

    deploy: 1862f6decf1d904caaea2f33b577ce0cf6144b48

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.5.5.dev36+g03052b585',
+    VERSION: '0.5.5.dev37+g1862f6dec',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
